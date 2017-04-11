56b3a0c2fae5d78af2167b2480ffa1c0d7ac929b
Yovalice Roman <yovalice@me.com>
2017-04-11 12:02:20 +0800

    Adding Spring project

---
+++ src/main/java/com/classicmodels/ClassicmodelsApplication.java
@@ -0,0 +1,12 @@
+package com.classicmodels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassicmodelsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassicmodelsApplication.class, args);
+	}
+}
---
+++ src/main/java/com/classicmodels/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.classicmodels;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ClassicmodelsApplication.class);
+	}
+
+}
